d05f8f08865bf51c0fbc393d8e1e2de1c10db61f
Gatzo03 <Danielebianchi.tobi@gmail.com>
2023-12-07 19:27:29 +0100

    aggiunto messaggio che dice se si sta scrivendo o cancellando

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
-const grid = document.querySelector(".container")
-for(let i = 0; i<(32); i++){
+const grid = document.querySelector(".container");
+const eraser = document.querySelector(".eraser");
+const body = document.querySelector("body");
+const containerMessaggio = document.createElement("div");
+const messaggioScrittura = document.createElement("h3");
+let erase=false;
+let write=false;
+for(let i = 0; i<(64); i++){
     const row = document.createElement("div");
     row.classList.add("riga");
     grid.appendChild(row);
-        for(let j=0;j<32;j++){
+        for(let j=0;j<64;j++){
         const square = document.createElement("div");
         row.appendChild(square);
         
@@ -13,22 +19,81 @@ for(let i = 0; i<(32); i++){
 }
 const squares = document.querySelectorAll(".square");
 
-let mouseClick = false;
-const body = document.querySelector("body");
-body.addEventListener('click', () => {
-    mouseClick = !mouseClick;
-    console.log(mouseClick);
+
+const griglia = document.querySelector(".omegaContainer");
+griglia.addEventListener('click', () => {
+    write = !write;
+    console.log("write= " + write);
+       Messaggio();
 })
+    /*if(write===true && erase===false){
+     
+        messaggioScrittura.textContent = "Stai scrivendo";
+        body.appendChild(containerMessaggio);
+        containerMessaggio.appendChild(messaggioScrittura);
+    } 
+    else if(write==false && erase==false){
+        containerMessaggio.remove();
+    }
+
+    else if( erase==true){
+        messaggioScrittura.textContent = "Stai cancellando";
+        body.appendChild(containerMessaggio);
+        containerMessaggio.appendChild(messaggioScrittura);
+
+    }
+})*/
 
 
 squares.forEach((square) => {
     
         square.addEventListener('mouseover', () =>{
-            if(mouseClick===true){
-            square.classList.add("blackSquare");
-            square.classList.remove("whiteSquare");
-    }})});
+            if(write===true && erase===false){
+                square.classList.add("blackSquare");
+                square.classList.remove("whiteSquare");
+            }
+            else if(write == true && erase===true){
+                square.classList.remove("blackSquare");
+                square.classList.add("whiteSquare");
+            }})});
 
+const cancelButton = document.querySelector(".eraser");
+cancelButton.addEventListener('click', () => {
+    erase=!erase;
+    console.log("erase= " + erase);
+    Messaggio();
+    
+});
+
+function Messaggio(){
+    if(write===true){
+        if(erase===false){
+            messaggioScrittura.textContent = "Stai scrivendo";
+            body.appendChild(containerMessaggio);
+            containerMessaggio.appendChild(messaggioScrittura);
+        }
+        else{
+            messaggioScrittura.textContent = "Stai cancellando";
+            body.appendChild(containerMessaggio);
+            containerMessaggio.appendChild(messaggioScrittura);
+        }
+    }
+    else{
+        if(erase===false){
+            messaggioScrittura.textContent = "puoi scrivere";
+            
+        }
+        else{
+            messaggioScrittura.textContent = "puoi cancellare";
+            
+        }
+    
+}
+}
+
+
+
+            
 
 
 
